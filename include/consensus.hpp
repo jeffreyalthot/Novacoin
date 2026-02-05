3512b898ce8f651fec488f7f4ee34db627323099
jeffreyalthot <jeffrey.althot@outlook.com>
2026-02-05 14:22:56 -0500

    Add mempool capacity limit with fee-based eviction

--- include/consensus.hpp
+++ include/consensus.hpp
@@ -13,6 +13,7 @@ inline constexpr Amount kMinRelayFee = 10'000;
 inline constexpr std::uint64_t kMaxFutureDriftSeconds = 2 * 60 * 60;
 inline constexpr std::uint64_t kTargetBlockTimeSeconds = 60;
 inline constexpr std::uint64_t kMempoolExpirySeconds = 24 * 60 * 60;
+inline constexpr std::size_t kMaxMempoolTransactions = 1'000;
 inline constexpr std::size_t kDifficultyAdjustmentInterval = 10;
 inline constexpr unsigned int kMinDifficulty = 1;
 inline constexpr unsigned int kMaxDifficulty = 2;
